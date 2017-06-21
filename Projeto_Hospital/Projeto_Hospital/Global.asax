bada92870ab022e6354100143da2cfcc44fcd602
1614833 <1614833@045LAB04013>
2017-06-21 10:30:26 -0300

    Add Projetos

---
+++ Projeto_Hospital/Projeto_Hospital/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Projeto_Hospital.MvcApplication" Language="C#" %>
---
+++ Projeto_Hospital/Projeto_Hospital/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Projeto_Hospital.Startup))]
+namespace Projeto_Hospital
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
